5cb2f788dd2264bcabaa7497745554aac805abc3
Toa Carney <toa@toacarney.nz>
2022-12-03 15:34:18 +1300

    Day 1, part 1

---
+++ day_1/part_1/calories.rb
@@ -0,0 +1,14 @@
+file = File.open("input.txt")
+str = file.read
+file.close
+
+elves = str.split "\n\n"
+
+largest_elf = 0
+elves.each { |elf|
+  total_calories = 0
+  elf.split.each { |food_item_str| total_calories += food_item_str.to_i }
+  if total_calories > largest_elf then largest_elf = total_calories end
+}
+
+puts "Largest elf has #{largest_elf} calories."
